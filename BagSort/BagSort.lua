f8b718af609fd400f099cdcb4553b43e4c799c54
Eterion <Eterion@users.noreply.github.com>
2020-06-23 06:50:12 +0200

    Update how bag assignments are saved

--- BagSort/BagSort.lua
+++ BagSort/BagSort.lua
@@ -1,20 +1,20 @@
-bagSlotFlags = {}
-bagAutoSortDisabled = {}
+BankAutosortDisabled = nil
+BackpackAutosortDisabled = nil
 
-local function GetBagAutoSortDisabled(id)
-    return bagAutoSortDisabled[id]
+local function SetBankAutosortDisabled(value)
+    BankAutosortDisabled = value
 end
 
-local function SetBagAutoSortDisabled(id, value)
-    bagAutoSortDisabled[id] = value
+local function SetBackpackAutosortDisabled(value)
+    BackpackAutosortDisabled = value
 end
 
-local function GetBagSlotFlag(id, value)
-    return bagSlotFlags[id] == value
+local function GetBankAutosortDisabled()
+    return BankAutosortDisabled
 end
 
-local function SetBagSlotFlag(id, value)
-    bagSlotFlags[id] = value
+local function GetBackpackAutosortDisabled()
+    return BackpackAutosortDisabled
 end
 
 -- Filters
@@ -55,21 +55,28 @@ local function ContainerFrameFilterDropDown_Initialize(self)
                 Flag.text = BAG_FILTER_LABELS[i]
                 Flag.func = function (_, _, _, value)
                     value = not value
+                    if (id > NUM_BAG_SLOTS) then
+                        SetBankBagSlotFlag(id - NUM_BAG_SLOTS, i ,value)
+                    else
+                        SetBagSlotFlag(id, i, value)
+                    end
                     if (value) then
                         parent.localFlag = i
-                        SetBagSlotFlag(id, parent.localFlag)
                         parent.FilterIcon.Icon:SetAtlas(BAG_FILTER_ICONS[i])
                         parent.FilterIcon:Show()
                     else
-                        parent.localFlag = -1
-                        SetBagSlotFlag(id, parent.localFlag)
                         parent.FilterIcon:Hide()
+                        parent.localFlag = -1
                     end
                 end
                 if (parent.localFlag) then
                     Flag.checked = parent.localFlag == i
                 else
-                    Flag.checked = GetBagSlotFlag(id, i)
+                    if (id > NUM_BAG_SLOTS) then
+                        Flag.checked = GetBankBagSlotFlag(id - NUM_BAG_SLOTS, i)
+                    else
+                        Flag.checked = GetBagSlotFlag(id, i)
+                    end
                 end
                 Flag.disabled = nil
                 Flag.tooltipOnButton = 1
@@ -91,9 +98,25 @@ local function ContainerFrameFilterDropDown_Initialize(self)
     local Ignore = UIDropDownMenu_CreateInfo()
     Ignore.text = BAG_FILTER_IGNORE
     Ignore.isNotRadio = true
-    Ignore.checked = GetBagAutoSortDisabled(id)
-    Ignore.func = function (_, _, _, checked)
-        SetBagAutoSortDisabled(id, not checked)
+    Ignore.func = function (_, _, _, value)
+        if (id == -1) then
+            SetBankAutosortDisabled(not value)
+        elseif (id == 0) then
+            SetBackpackAutosortDisabled(not value)
+        elseif (id > NUM_BAG_SLOTS) then
+            SetBankBagSlotFlag(id - NUM_BAG_SLOTS, LE_BAG_FILTER_FLAG_IGNORE_CLEANUP, not value)
+        else
+            SetBagSlotFlag(id, LE_BAG_FILTER_FLAG_IGNORE_CLEANUP, not value)
+        end
+    end
+    if (id == -1) then
+        Ignore.checked = GetBankAutosortDisabled()
+    elseif (id == 0) then
+        Ignore.checked = GetBackpackAutosortDisabled()
+    elseif (id > NUM_BAG_SLOTS) then
+        Ignore.checked = GetBankBagSlotFlag(id - NUM_BAG_SLOTS, LE_BAG_FILTER_FLAG_IGNORE_CLEANUP)
+    else
+        Ignore.checked = GetBagSlotFlag(id, LE_BAG_FILTER_FLAG_IGNORE_CLEANUP)
     end
     UIDropDownMenu_AddButton(Ignore)
 end
@@ -119,13 +142,17 @@ local function ContainerFramePortraitButton_OnEnter(self)
 end
 
 local function ContainerFramePortraitButton_OnLeave(self)
-    if (self:GetID() >= 0) then
+    local parent = self:GetParent()
+    local id = parent:GetID()
+    if (id >= 0) then
         self.Highlight:Hide()
     end
 end
 
 local function ContainerFramePortraitButton_OnClick(self)
-    if (self:GetID() >= 0) then
+    local parent = self:GetParent()
+    local id = parent:GetID()
+    if (id >= 0) then
         local parent = self:GetParent()
         PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
         ToggleDropDownMenu(1, nil, parent.FilterDropDown, self:GetName(), 0, 0)
