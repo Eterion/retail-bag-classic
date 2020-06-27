5ea7ad1ff2b67dd491198869a6584b81299cbe84
Eterion <Eterion@users.noreply.github.com>
2020-06-27 13:29:52 +0200

    Replace bag free slots with blizzard ui

--- FreeSlots/FreeSlots.lua
+++ FreeSlots/FreeSlots.lua
@@ -1,41 +1,8 @@
--- Event frame
-local EventFrame = CreateFrame("Frame")
+SetCVar("displayFreeBagSlots", "1", MainMenuBarBackpackButton)
+local BACKPACK_FREESLOTS_FORMAT = "(%s)"
 
--- Label frame
-local FreeSlotsLabel = MainMenuBarBackpackButton:CreateFontString("FreeSlotsLabel", "OVERLAY", "GameTooltipText")
-FreeSlotsLabel:SetFont("Fonts\\ARIALN.TTF", 14, "THINOUTLINE")
-FreeSlotsLabel:SetPoint("BOTTOM", 0, 3)
-FreeSlotsLabel:SetTextColor(1, 1, 1)
-
--- Return number of total free slots
-local function CountFreeSlots()
-    local totalFreeSlots = 0
-    for i = 0, NUM_BAG_SLOTS do
-        local freeSlots, bagType = GetContainerNumFreeSlots(i)
-        if (bagType == 0) then
-            totalFreeSlots = totalFreeSlots + freeSlots
-        end
-    end
-    return totalFreeSlots
-end
-
--- Update label
-local function UpdateLabel()
-    FreeSlotsLabel:SetFormattedText("(%d)", CountFreeSlots())
-end
-
--- Event hook
-local function OnEvent(self, event, bagId)
-    if (bagId >= 0 and bagId <= NUM_BAG_SLOTS) then
-        UpdateLabel()
-    end
-end
-
--- Register all events
-local function RegisterEvents()
-    EventFrame:SetScript("OnEvent", OnEvent)
-    EventFrame:RegisterEvent("BAG_UPDATE")
-end
-
--- Init
-RegisterEvents()
+-- Update total free slots
+hooksecurefunc("MainMenuBarBackpackButton_UpdateFreeSlots", function ()
+    local totalFree = CalculateTotalNumberOfFreeBagSlots()
+	MainMenuBarBackpackButtonCount:SetText(BACKPACK_FREESLOTS_FORMAT:format(totalFree))
+end)
